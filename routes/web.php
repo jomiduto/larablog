1975982d563518167f22b0d80ba5155f381befcc
Jose Dueñas <jomiguel097@gmail.com>
2021-07-22 14:14:50 -0500

    Ejemplo de rutas con Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpKernel\Profiler\Profile;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    return "Prueba ruta con Laravel. Perfil: <a href='" . route("profile") . "'>clic aquí</a>";
+});
+
+Route::get('/user/profile', function () {
+    return "<h1>Ejemplo de Rutas con Nombres</h1>";
+})->name('profile');
