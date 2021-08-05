09f166bd382ce4cca02c7c6b72e92c6e4875a944
Jose Dueñas <jomiguel097@gmail.com>
2021-08-05 15:51:30 -0500

    Instalación de Laravel UI, instalación de los paquetes de node npm install && npm run dev. Necesarios para incluir js y css en el proyecto

--- resources/views/dashboard/post/create.blade.php
+++ resources/views/dashboard/post/create.blade.php
@@ -5,23 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Creación</title>
+    <link rel="stylesheet" href="{{ asset("css/app.css") }}"> <!-- Se realiza el enlace con la carpeta public/css/app.css -->
+    <script src="{{ asset("js/app.js") }}"></script> <!-- Se realiza el enlace con la carpeta public/js/app.js -->
 </head>
 <body>
-    <form action="{{ route("post.store") }}" method="POST">
-        @csrf
-        <div class="form-group">
-            <label for="title">Título</label>
-            <input type="text" class="form-control" name="title" id="title" autocomplete="off" placeholder="Título">
-        </div>
-        <div class="form-group">
-            <label for="url_clean">URL limpia</label>
-            <input type="text" class="form-control" name="url_clean" id="url_clean" autocomplete="off" placeholder="URL limpia">
-        </div>
-        <div class="form-group">
-            <label for="content">Contenido</label>
-            <textarea class="form-control" name="content" id="contenido" rows="3" placeholder="Contenido del post"></textarea>
-        </div>
-        <button type="submit" class="btn btn-primary">Enviar</button>
-    </form>
+    <div class="container">
+        <h1>Crear Post</h1>
+        <br>
+        <form action="{{ route("post.store") }}" method="POST">
+            @csrf
+            <div class="form-group">
+                <label for="title">Título</label>
+                <input type="text" class="form-control" name="title" id="title" autocomplete="off" placeholder="Título">
+            </div>
+            <div class="form-group">
+                <label for="url_clean">URL limpia</label>
+                <input type="text" class="form-control" name="url_clean" id="url_clean" autocomplete="off" placeholder="URL limpia">
+            </div>
+            <div class="form-group">
+                <label for="content">Contenido</label>
+                <textarea class="form-control" name="content" id="contenido" rows="3" placeholder="Contenido del post"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+    </div>
 </body>
 </html>
