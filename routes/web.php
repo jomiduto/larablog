e8087283afd20d85a6682b3e980619cd26b3dadb
Jose Dueñas <jomiguel097@gmail.com>
2021-08-27 14:34:05 -0500

    Creación CRUD para las categorías, creación del controlador y página de registro de categoría

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\dashboard\CategoryController;
 use App\Http\Controllers\dashboard\PostController;
 use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
@@ -20,36 +21,8 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-// Route::get('/test', function () {
-//     return "Prueba ruta con Laravel. Perfil: <a href='" . route("profile") . "'>clic aquí</a>";
-// });
-
-// Route::get('/user/profile', function () {
-//     return "<h1>Ejemplo de Rutas con Nombres</h1>";
-// })->name('profile');
-
-// // Creación de ruta - Opción 1 con parámetros
-
-// // Route::get('home', function () {
-// //     $nombre = "Jose Miguel Dueñas Torres";
-// //     return view("home")->with("nombre", $nombre); // Se especifica la vista que se quiere retornar, esta en la carpeta /resources/views
-// // })->name("home");
-
-// // Opción 2 con parámetros
-
-// Route::get('home1/{nombre?}/{apellido?}', function ($nombre = "Lizet Daniela", $apellido = "Ospina Castro") {
-
-//     return view("home", ["nombre" => $nombre, "apellido" => $apellido]);
-// })->name("home");
-
-// Route::get('/clase', function () {
-//     $posts = ["Post 1", "Post 2", "Post 3", "Post 4"];
-
-//     return view("clase", ['posts' => $posts]);
-// })->name("clase");
-
-// Ruta a un controlador PostController
-// Route::get('post', [PostController::class, 'index']);
-
 // Ruta de tipo recurso para el controlador PostController
 Route::resource('dashboard/post', PostController::class);
+
+// Ruta para controllador CategoryController
+Route::resource('dashboard/category', CategoryController::class);
