4bd2388fa16eb6c3529b716ae1160d4e0de766d9
Jose Dueñas <jomiguel097@gmail.com>
2021-09-16 17:40:36 -0500

    modificación estructura tabla usuarios, campo rol_id con default 2

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('rol_id');
+            $table->string('rol_id')->default(2);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
