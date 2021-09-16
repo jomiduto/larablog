8b2b9791c08852612395e01eefd0cde1802f97f6
Jose Dueñas <jomiguel097@gmail.com>
2021-09-16 14:04:12 -0500

    Proteger módulo de dashboard con login mediante un middleware (V.D)

--- app/Http/Controllers/dashboard/PostController.php
+++ app/Http/Controllers/dashboard/PostController.php
@@ -12,6 +12,10 @@ use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Listar elementos
      *
